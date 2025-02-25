7a2f5560eedc686f2d932eb3186edcf93992a058
cloudkim2025 <cloudviola@icloud.com>
2025-02-25 11:11:19 +0900

    Prj setup

---
+++ springboot/examplespringboot/src/test/java/com/example/examplespringboot/ExamplespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplespringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
