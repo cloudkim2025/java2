82925a35edf4ecffbf7e0201bceb4dcde417b83c
cloudkim2025 <cloudviola@icloud.com>
2025-03-05 09:47:02 +0900

    basic-board-v2

---
+++ springboot/springboot/tokenexample/src/main/java/com/example/tokenexample/TokenexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tokenexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenexampleApplication.class, args);
+	}
+
+}
---
+++ springboot/springboot/tokenexample/src/test/java/com/example/tokenexample/TokenexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tokenexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
